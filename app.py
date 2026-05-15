9b76e5911a114cd6c34cfbb40bad0799613aad4f
shoukeen ahmad <58034880+shoukeen-ahmad@users.noreply.github.com>
2026-05-15 17:43:58 +0530

    Update greeting message in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/a')
 def home():
-    return 'Hello, from Flask and Docker World!'
+    return 'Hello Dosto, from Flask and Docker World!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
